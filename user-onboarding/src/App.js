52ae1ab1fe1cd8bba48827d765a0df66d8c685dd
unknown <xiongjoh@gmail.com>
2020-10-14 14:48:27 -0500

    add onChange and controls to inputs Form.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -9,18 +9,49 @@ const initialFormValues = {
   tos: false,
 }
 
+const initialFormErrors = {
+  name:'',
+  email:'',
+  password:'',
+}
+
 const initialUsers = []
+const initialDisabled = true
 
 function App() {
 
 const [users, setUsers] = useState(initialUsers)
 const [formValues, setFormValues] = useState(initialFormValues)
+const [formErrors, setFormErrors] = useState(initialFormErrors)
+const [disabled, setDisabled] = useState(initialDisabled)
 
+// const postUser = (newUser) => {
+//   axios.post(``)
+// }
+
+const change = (name, value) => {
+  setFormValues({...formValues, [name]: value})
+}
+
+const submit = () => {
+  const newUser = {
+    name: formValues.name.trim(),
+    email: formValues.email.trim(),
+    password: formValues.password.trim(),
+  }
+
+}
 
 
   return (
     <div className="App">
-      <Form />
+      <Form 
+      values={formValues}
+      change={change}
+      submit={submit}
+      disable={disabled}
+      errors={formErrors}
+      />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,28 +2,62 @@ import React from "react";
 
 export default function Form(props) {
   const { values, submit, change, disable, error } = props;
-  
+
+  const onChange = (event) => {
+    //   debugger
+      const {value, type, name, checked} = event.target
+      const valueToUse = type === 'checkbox' ? checked : value
+      change(name, valueToUse)
+  }
+
+  const onSubmit = () => {
+
+  }
+
   return (
-    <form className="container">
+    <form className="container" onSubmit={onSubmit}>
       <label>
         Name:
-        <input type="text" />
+        <input 
+        type="text"
+        name="name"
+        value={values.name}
+        onChange={onChange}
+        />
       </label>
 
       <label>
         Email:
-        <input type="email" />
+        <input 
+        type="email"
+        name="email"
+        value={values.email}
+        onChange={onChange}
+        />
       </label>
 
       <label>
         Password:
-        <input type="text" />
+        <input 
+        type="text"
+        name="password"
+        value={values.password}
+        onChange={onChange}
+        />
       </label>
 
       <label>
         Terms Of Service
-        <input type="checkbox" />
+        <input 
+        type="checkbox"
+        name="tos"
+        checked={values.hiking}
+        onChange={onChange}
+        />
       </label>
+      <div>
+          <button disabled={disable}>Submit</button>
+      </div>
     </form>
   );
 }
