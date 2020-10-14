8a7a61998ec2c3bdd8d7660978da927a133ea39b
unknown <xiongjoh@gmail.com>
2020-10-14 14:26:06 -0500

    add name, email, password, and tos label/input to Form.js

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function Form (props) {
-    return null
+export default function Form(props) {
+  const { values, submit, change, disable, error } = props;
+  
+  return (
+    <form className="container">
+      <label>
+        Name:
+        <input type="text" />
+      </label>
+
+      <label>
+        Email:
+        <input type="email" />
+      </label>
+
+      <label>
+        Password:
+        <input type="text" />
+      </label>
+
+      <label>
+        Terms Of Service
+        <input type="checkbox" />
+      </label>
+    </form>
+  );
 }
